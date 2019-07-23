54bfb6b1c12aa537e94dd8f73dd59f5260796263
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-07-23 18:55:16 -0400

    completed callbacks assignment and some of arrays

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,51 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach (function (item) {
+fullName.push (item.first_name + ' ' + item.last_name);
+
+})
+
+
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+let allCaps = runners.map(function(item) {
+
+return {
+    "id": item.id,
+    "first_name": item.first_name.toUpperCase(),
+    "last_name": item.last_name,
+    "email":item.email,
+    "shirt_size":item.shirt_size,
+    "company_name":item.company_name,
+    "donation":item.donation,
+}
+});
+
+
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+let largeShirts = runners.filter (function(item){
+    return item.shirt_size === "L";
+});
+
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+let ticketPriceTotal = [runners.donation].reduce(function(accumulator,currentItem){
+
+return accumulator + currentItem;
+      },0)
+      
+    
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,7 +2,7 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+ 
 
   //Given this problem: 
   
@@ -22,29 +22,61 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     console.log(first)
   });
 
-*/
+  function log (param){
+console.log(param);
+  }
 
 
-function getLength(arr, cb) {
+  function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
+
+function getLength(arr, cb) {
+  return cb(arr.length);
+}
+
+getLength(items, log); 
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
 
+function last(arr, cb) {
+  cb(arr[arr.length - 1]);
+}
+last(items, log); 
+
+
+
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+cb (x + y);
 }
+sumNums(5,5,log);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+ cb (x * y);
 }
+
+multiplyNums (5,5, log);
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+let isPresent = false;
+ 
+list.forEach(function (currentItem){
+  if (currentItem === item) {
+    isPresent = true;
+  }
+})
+cb (isPresent);
 }
+ contains ("Pencil",  items, log );
+
 
 /* STRETCH PROBLEM */
 
